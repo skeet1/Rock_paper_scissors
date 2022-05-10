d88a78a6dc6e5b25a1df9223bd494ab0eed8f143
Mohamed Karim <76565545+skeet1@users.noreply.github.com>
2022-05-10 13:24:11 +0100

    done with a new design

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// let user_score = document.getElementById("user_score");
+// let comp_score = document.getElementById("comp_score");
+// let result = document.getElementById("result");
+
+function comp_turn() {
+	return (Math.ceil(Math.random() * 3));
+}
+
+
